831d76686cfad92e68590db2c7eb9ffa157ae6d4
Thorsten Reiser <thorsten.reiser@dinited.com>
2025-04-10 16:07:48 +0200

    copy hidden directories also

--- modules/wordpress/main.tf
+++ modules/wordpress/main.tf
@@ -149,7 +149,7 @@ resource "coder_agent" "wordpress" {
     cd /var/www/
     mkdir -p temp_dir
     git clone --single-branch --branch ${var.branch} ${var.repo} temp_dir
-    mv temp_dir/* /var/www/html
+    mv temp_dir/. /var/www/html
     rm -rf temp_dir --no-preserve-root
     cd /var/www/html
 
